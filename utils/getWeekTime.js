9e4a8331c04b41be29107a021365ef6125f07cfa
lxy-Jason <1627962483@qq.com>
2022-09-03 23:09:27 +0800

    时间记录精确到秒

--- utils/getWeekTime.js
+++ utils/getWeekTime.js
@@ -3,13 +3,17 @@ const tiemFormat = require('./timeFormat')
 const getWeekTime = (arr) => {
   let hours = 0;
   let minutes = 0;
+  let seconds = 0;
   arr.forEach(item => {
     let temp = item.time.split(':')
     hours += Number(temp[0])
     minutes += Number(temp[1])
+    seconds += Number(temp[2])
   })
   hours += Math.floor(minutes / 60)
+  minutes += Math.floor(seconds / 60)
   minutes = minutes % 60
-  return tiemFormat(hours, minutes)
+  seconds = seconds % 60
+  return tiemFormat(hours, minutes,seconds)
 }
 module.exports = getWeekTime
