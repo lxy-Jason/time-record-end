db68992e112bd6c29e3a9bd8f4146c481ae3ee1e
lxy-Jason <1627962483@qq.com>
2022-09-12 15:41:30 +0800

    添加修改用户名功能

--- controller/users.js
+++ controller/users.js
@@ -3,31 +3,31 @@ let jwt = require('jsonwebtoken')
 
 //用户登录
 const login = async ctx => {
-  let {username,password} = ctx.request.body
-  await Users.findOne({username,password}).then(rel => {
-    if(rel){
+  let { username, password } = ctx.request.body
+  await Users.findOne({ username, password }).then(rel => {
+    if (rel) {
       let token = jwt.sign(
-        {username: rel.username},
+        { username: rel.username },
         'time-record-server-jwt',
-        {expiresIn:3600 * 24 * 30}
+        { expiresIn: 3600 * 24 * 30 }
       )
       ctx.body = {
-        code:200,
-        msg:'登录成功',
+        code: 200,
+        msg: '登录成功',
         username,
         token
       }
     }
-    else{
+    else {
       ctx.body = {
-        code:406,
-        msg:'用户名或密码错误'
+        code: 406,
+        msg: '用户名或密码错误'
       }
     }
   }).catch(err => {
     ctx.body = {
-      code:500,
-      msg:'服务端错误,登录异常',
+      code: 500,
+      msg: '服务端错误,登录异常',
       err
     }
   })
@@ -35,44 +35,81 @@ const login = async ctx => {
 
 //用户注册
 const register = async ctx => {
-  let {username,password} = ctx.request.body
+  let { username, password } = ctx.request.body
   let isDouble = false
 
-  await Users.findOne({username}).then(rel => {
-    if(rel) isDouble = true  
+  await Users.findOne({ username }).then(rel => {
+    if (rel) isDouble = true
   })
-  if(isDouble){
+  if (isDouble) {
     ctx.body = {
-      code:406,
-      msg:'用户已存在'
+      code: 406,
+      msg: '用户已存在'
     }
     return
   }
 
-  await Users.create({username,password}).then(rel => {
-    if(rel){
+  await Users.create({ username, password }).then(rel => {
+    if (rel) {
       let token = jwt.sign(
-        {username: rel.username},
+        { username: rel.username },
         'time-record-server-jwt',
-        {expiresIn:3600 * 24 * 30}
+        { expiresIn: 3600 * 24 * 30 }
       )
       ctx.body = {
-        code:201,
-        msg:'注册成功',
+        code: 201,
+        msg: '注册成功',
         username,
         token
       }
     }
-    else{
+    else {
       ctx.body = {
-        code:406,
-        msg:'注册失败'
+        code: 406,
+        msg: '注册失败'
       }
     }
   }).catch(err => {
     ctx.body = {
-      code:500,
-      msg:'服务器错误,注册失败',
+      code: 500,
+      msg: '服务器错误,注册失败',
+      err
+    }
+  })
+}
+//修改用户名
+const usernameUpdate = async ctx => {
+  let { username, newName } = ctx.request.body
+  console.log(username, newName);
+
+  let isDouble = false
+  await Users.findOne({ username }).then(rel => {
+    if (rel) isDouble = true
+  })
+  if (isDouble) {
+    ctx.body = {
+      code: 406,
+      msg: '用户名已存在'
+    }
+    return
+  }
+  await Users.updateOne({ "username": username }, { $set: { "username": newName } }).then(rel => {
+    if (rel) {
+      ctx.body = {
+        code: 200,
+        msg: '修改用户名成功'
+      }
+    }
+    else {
+      ctx.body = {
+        code: 300,
+        msg: '修改用户名失败'
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 500,
+      msg: '服务端错误',
       err
     }
   })
@@ -80,5 +117,6 @@ const register = async ctx => {
 
 module.exports = {
   login,
-  register
+  register,
+  usernameUpdate
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')()
-const {login,register} = require('../controller/users')
+const {login,register,usernameUpdate} = require('../controller/users')
 router.prefix('/users')
 
 //用户登录
@@ -7,5 +7,8 @@ router.post('/login',login)
 
 //用户注册
 router.post('/register',register)
+
+//修改用户名
+router.post('/usernameUpdate',usernameUpdate)
 
 module.exports = router
