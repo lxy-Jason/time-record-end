fcab15668edbe8d2214be6bb95d44c943895abec
lxy-Jason <1627962483@qq.com>
2023-08-27 15:51:37 +0800

    fix: 数据库连接问题

--- db/index.js
+++ db/index.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: xiangyue_li
+ * @Date: 2023-08-27 15:07:17
+ * @LastEditors: xiangyue_li
+ */
 const mongoose = require('mongoose');
 
 main().then(() => {
@@ -6,7 +11,7 @@ main().then(() => {
 .catch(err =>console.log('数据库连接失败',err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/time-record');
+  await mongoose.connect('mongodb://127.0.0.1:27017/time-record'); //用localhost连不上,得用127.0.0.1
 }
 
 module.exports = main
